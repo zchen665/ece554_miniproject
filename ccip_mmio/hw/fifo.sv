9879a9f3150fef7f91479e60dec2ad8590bc4984
zchen665 <azchen665@gmail.com>
2021-09-09 16:59:17 -0500

    fifo done

--- ccip_mmio/hw/fifo.sv
+++ ccip_mmio/hw/fifo.sv
@@ -11,7 +11,26 @@ module fifo
   (
   input clk,rst_n,en,
   input [BITS-1:0] d,
-  output [BITS-1:0] q
+  output logic [BITS-1:0] q
   );
-  // your RTL code here
+
+  logic [DEPTH-1:0]queue [BITS-1:0];
+  // logic bitsout[BITS-1:0];
+  always@(posedge clk, negedge rst_n) begin
+    if !(rst_n) begin
+      for(int i = 0; i < DEPTH-2; i++) begin
+        queue[DEPTH-1-i] <= 0;
+      end
+      queue[0]<= 0
+    end
+    if (en) begin
+      q <= queue[DEPTH-1];
+      for(int i = 0; i < DEPTH-2; i++) begin
+        queue[DEPTH-1-i] <= queue[DEPTH-2-i];
+      end
+      queue[0]<= d;
+    end
+  end
+
+
 endmodule // fifo
